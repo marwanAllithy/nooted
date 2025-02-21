dca945c1fae4f0f3c1c962abc836187601ba64f4
Marwan Allisy <92400495+marwanAllithy@users.noreply.github.com>
2025-02-21 10:35:00 +0800

    foundations of auto comp

--- apps/frontend/src/components/Editor/BlockElement.tsx
+++ apps/frontend/src/components/Editor/BlockElement.tsx
@@ -1,6 +1,6 @@
 import Block from "@/lib/Editor/Block";
 import { BlockType } from "@/types/editor";
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {
  blocks: Block[];
@@ -19,6 +19,8 @@ export default function BlockElement({
  inputRefs,
  editor,
 }: Props) {
+ const [showAutoComplete, setShowAutoComplete] = useState(false);
+
  const handleKeyDown = (
   event: React.KeyboardEvent<HTMLDivElement>,
   currLevel: number
@@ -27,15 +29,21 @@ export default function BlockElement({
   switch (event.key) {
    case "Enter":
     event.preventDefault();
+    setShowAutoComplete(false);
     editor.addBlock(blocks, setBlocks, BlockType.TEXT, "", currLevel);
     break;
    case "ArrowUp":
+    setShowAutoComplete(false);
     editor.moveFocusUp(currLevel, inputRefs);
     break;
    case "ArrowDown":
+    setShowAutoComplete(false);
     editor.moveFocusDown(currLevel, inputRefs);
     break;
    case "/":
+    setShowAutoComplete(true);
+
+  // TODO: look for the search term that is written right after the / then look for it using our arrays to find the respective block type  
     // Handle popup logic here
     break;
    default:
@@ -43,16 +51,24 @@ export default function BlockElement({
   }
  };
  return (
-  <div
-   key={block?.level}
-   ref={(el) => {
-    inputRefs.current[index] = el;
-   }}
-   contentEditable
-   suppressContentEditableWarning={true}
-   onKeyDown={(e) => handleKeyDown(e, block.level)}
-  >
-   {block.data.text}
-  </div>
+  <>
+   <div
+    className="prose relative "
+    key={block?.level}
+    ref={(el) => {
+     inputRefs.current[index] = el;
+    }}
+    contentEditable
+    suppressContentEditableWarning={true}
+    onKeyDown={(e) => handleKeyDown(e, block.level)}
+   >
+    {block.data.text}
+   </div>
+   {showAutoComplete && (
+    <div className="absolute z-20 ">
+     <div>AutoComplete</div>
+    </div>
+   )}
+  </>
  );
 }
